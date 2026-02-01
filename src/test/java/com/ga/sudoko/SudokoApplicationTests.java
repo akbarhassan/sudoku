5d7b484c24bcce12117b92e04082ad04c3ccd89c
akbar Alsaleh <akbaralsaleh@akbars-MacBook-Air.local>
2026-02-01 19:48:18 +0300

    new sudoku project

---
+++ src/test/java/com/ga/sudoko/SudokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.sudoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
